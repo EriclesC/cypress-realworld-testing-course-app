45d81f0c9d26f4499907bfd7d91281704a64f3e7
Ericles Carvalho <ericlescarvalho77@gmail.com>
2025-07-15 13:04:05 -0300

    refactor: clean up spacing in course section tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -18,20 +18,20 @@ describe("home page", () => {
   }),
 
     context("Courses section", () => {
+
         it.only("Course: Testing Your First Next.js Application", () => {
           cy.getByData("course-0").find("a").contains("Get started").click()
           cy.location("pathname").should("equal", "/testing-your-first-application")
         })
+
         it.only("Course: Testing Foundations", () => {
           cy.getByData("course-1").find("a").contains("Get started").click()
           cy.location("pathname").should("equal", "/testing-foundations")
         })
+
         it.only("Course: Testing Foundations", () => {
           cy.getByData("course-2").find("a").contains("Get started").click()
           cy.location("pathname").should("equal", "/cypress-fundamentals")
         })
     })
-
-
-
 })
